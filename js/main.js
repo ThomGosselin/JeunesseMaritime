572ad41ad648c16df7a5861ba32cdc6c3192ffe7
Thomas Gosselin <1961579@cgmatane.qc.ca>
2021-11-12 14:01:59 -0500

    ajout des photos partenaires + apparence menu terminer + code general de la page d'accueil terminer + début du css des diffrentres section

--- js/main.js
+++ js/main.js
@@ -1,9 +1,71 @@
 const menuBtn = document.querySelectorAll(".dropbtn");
 const dropItem = document.querySelectorAll(".drop-box");
 
+let isBtnActive = false;
+let oldID = null;
+
+let isBtnOut = false;
+let isDropOut = false;
+
 menuBtn.forEach((btn, id) => {
     btn.addEventListener("click", () => {
-        console.log(id);
+
+        if(id != oldID && oldID != null) {
+            dropItem[oldID].classList.remove("show");
+            isBtnActive = false;
+        }
+
         dropItem[id].classList.add("show");
+
+        if(isBtnActive) {
+        dropItem[id].classList.remove("show");
+        isBtnActive = false;
+        return;
+        };
+
+        isBtnActive = true;
+        oldID = id;
     })
 });
+
+menuBtn.forEach((btn, id) => {
+    btn.addEventListener("mouseover", () => {
+
+        if(id != oldID && oldID != null) {
+            dropItem[oldID].classList.remove("show");
+            isBtnActive = false;
+        }
+
+        dropItem[id].classList.add("show");
+
+        if(isBtnActive) {
+        dropItem[id].classList.remove("show");
+        isBtnActive = false;
+        return;
+        };
+
+        isBtnActive = true;
+        oldID = id;
+
+        btn.addEventListener("mouseout", () => {
+            isBtnOut = true;
+            removeDropDownMenu(id);
+        })
+
+        dropItem[id].addEventListener("mouseout", () => {
+            isDropOut = true;
+            removeDropDownMenu(id);
+        })
+    })
+});
+
+function removeDropDownMenu(id) { 
+    console.log(isDropOut, isBtnOut);
+    if(isDropOut && isBtnOut){
+        dropItem[id].classList.remove("show");
+        isBtnActive = false;
+    };
+
+    isBtnOut = false;
+    isDropOut = false;
+}
