0ecaebd54ae7909a22702a1028dc22990ca56980
Nathan Woods <1933156@cgmatane.qc.ca>
2021-11-15 16:09:24 -0500

    Test Menu

--- js/main.js
+++ js/main.js
@@ -25,13 +25,59 @@ menuBtn.forEach((btn, id) => {
 
   btn.addEventListener("mouseleave", (evt) => {
     let btnPos = dropItem[id].getBoundingClientRect();
-    let btnCenterX = (btnPos.left + btnPos.right) / 2;
-    let btnCenterY = (btnPos.top + btnPos.bottom) / 2;
-    console.log(btnCenterX, btnCenterY);
-    console.log(evt);
-    /*if (!isDropMenuOpen) {
-      dropItem[id].classList.remove("show");
-      isDropMenuOpen = false;
-    }*/
+    let currentSide = closestSide(
+      evt.pageX - btnPos.left,
+      evt.pageY - btnPos.top - window.pageYOffset,
+      dropItem[id].offsetWidth,
+      dropItem[id].offsetHeight
+    );
+
+    switch (currentSide) {
+      case "bottom":
+        console.log("Bottom");
+        break;
+      case "top":
+        console.log("Top");
+        dropItem[id].classList.remove("show");
+        isDropMenuOpen = false;
+        break;
+      case "left":
+        console.log("Left");
+        dropItem[id].classList.remove("show");
+        isDropMenuOpen = false;
+        break;
+      case "right":
+        console.log("Right");
+        dropItem[id].classList.remove("show");
+        isDropMenuOpen = false;
+        break;
+      default:
+        break;
+    }
   });
 });
+
+function closestSide(x, y, w, h) {
+  let topSideDist = distMetric(x, y, w / 2, 0);
+  let bottomSideDist = distMetric(x, y, w / 2, h);
+  let leftSideDist = distMetric(x, y, 0, h / 2);
+  let rightSideDist = distMetric(x, y, w, h / 2);
+
+  let min = Math.min(topSideDist, bottomSideDist, leftSideDist, rightSideDist);
+  switch (min) {
+    case leftSideDist:
+      return "left";
+    case rightSideDist:
+      return "right";
+    case topSideDist:
+      return "top";
+    case bottomSideDist:
+      return "bottom";
+  }
+}
+
+function distMetric(x, y, x2, y2) {
+  let xDiff = x - x2;
+  let yDiff = y - y2;
+  return xDiff * xDiff + yDiff * yDiff;
+}
