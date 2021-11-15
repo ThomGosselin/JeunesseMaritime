caffdf6ac2405e3fe79e6a06d6e4379284d0cc93
Nathan Woods <1933156@cgmatane.qc.ca>
2021-11-15 17:28:57 -0500

    Menu Js Final

--- js/main.js
+++ js/main.js
@@ -1,83 +1,39 @@
 const menuBtn = document.querySelectorAll(".dropbtn");
-const dropItem = document.querySelectorAll(".drop-box");
+const dropItems = document.querySelectorAll(".drop-box");
+const menu = document.querySelector(".menu");
 
-let isMouseOnDropMenu = false;
-let isDropMenuOpen = false;
+
+let showIndex = 0;
 
 menuBtn.forEach((btn, id) => {
+
   btn.addEventListener("mouseenter", () => {
-    if (!isDropMenuOpen) {
-      dropItem[id].classList.add("show");
-      isDropMenuOpen = true;
-    }
-
-    dropItem[id].addEventListener("mouseenter", () => {
-      isMouseOnDropMenu = true;
-    });
-
-    dropItem[id].addEventListener("mouseleave", () => {
-      if (isMouseOnDropMenu) {
-        dropItem[id].classList.remove("show");
-        isDropMenuOpen = false;
-      }
-    });
+    showIndex++
+    updateDropDownMenu(dropItems[id]);
   });
 
-  btn.addEventListener("mouseleave", (evt) => {
-    let btnPos = dropItem[id].getBoundingClientRect();
-    let currentSide = closestSide(
-      evt.pageX - btnPos.left,
-      evt.pageY - btnPos.top - window.pageYOffset,
-      dropItem[id].offsetWidth,
-      dropItem[id].offsetHeight
-    );
+  dropItems[id].addEventListener("mouseenter", () => {
+      showIndex++;
+    });
 
-    switch (currentSide) {
-      case "bottom":
-        console.log("Bottom");
-        break;
-      case "top":
-        console.log("Top");
-        dropItem[id].classList.remove("show");
-        isDropMenuOpen = false;
-        break;
-      case "left":
-        console.log("Left");
-        dropItem[id].classList.remove("show");
-        isDropMenuOpen = false;
-        break;
-      case "right":
-        console.log("Right");
-        dropItem[id].classList.remove("show");
-        isDropMenuOpen = false;
-        break;
-      default:
-        break;
-    }
-  });
+    dropItems[id].addEventListener("mouseleave", () => {
+        showIndex--;
+        updateDropDownMenu(dropItems[id]);
+    });
+
+    btn.addEventListener("mouseleave", (evt) => {
+      showIndex--;
+      updateDropDownMenu(dropItems[id]);
+    });
 });
 
-function closestSide(x, y, w, h) {
-  let topSideDist = distMetric(x, y, w / 2, 0);
-  let bottomSideDist = distMetric(x, y, w / 2, h);
-  let leftSideDist = distMetric(x, y, 0, h / 2);
-  let rightSideDist = distMetric(x, y, w, h / 2);
-
-  let min = Math.min(topSideDist, bottomSideDist, leftSideDist, rightSideDist);
-  switch (min) {
-    case leftSideDist:
-      return "left";
-    case rightSideDist:
-      return "right";
-    case topSideDist:
-      return "top";
-    case bottomSideDist:
-      return "bottom";
-  }
-}
-
-function distMetric(x, y, x2, y2) {
-  let xDiff = x - x2;
-  let yDiff = y - y2;
-  return xDiff * xDiff + yDiff * yDiff;
+function updateDropDownMenu(dropItem) {
+  setTimeout(() => {
+    dropItems.forEach(item => {
+      item.classList.remove("show");
+    });
+    if (showIndex > 0) {
+      dropItem.classList.add("show");
+    }
+  }, 1);
 }
