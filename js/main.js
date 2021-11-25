b7989894e29df8dca934842dfa7e5b02556afbb0
Nathan Woods <1933156@cgmatane.qc.ca>
2021-11-24 21:50:23 -0500

    Fin Header, Ajout Image Fond, Modification Menu, Finition Equipage

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 const menuBtn = document.querySelectorAll(".dropbtn");
 const dropItems = document.querySelectorAll(".drop-box");
 const menu = document.querySelector(".menu");
-
 const logoMenu = document.querySelector(".logo-menu");
 
 ////////////////////////////////
@@ -12,34 +11,55 @@ let showIndex = 0;
 menuBtn.forEach((btn, id) => {
   btn.addEventListener("mouseenter", () => {
     showIndex++;
+    btn.classList.add("green");
     updateDropDownMenu(dropItems[id]);
   });
 
   dropItems[id].addEventListener("mouseenter", () => {
     showIndex++;
+    setTimeout(() => {
+      btn.classList.add("green");
+    }, 1);
   });
 
   dropItems[id].addEventListener("mouseleave", () => {
     showIndex--;
-    updateDropDownMenu(dropItems[id]);
+    updateDropDownMenu(dropItems[id], btn);
   });
 
-  btn.addEventListener("mouseleave", (evt) => {
+  btn.addEventListener("mouseleave", () => {
     showIndex--;
-    updateDropDownMenu(dropItems[id]);
+    updateDropDownMenu(dropItems[id], btn);
   });
 });
 
-function updateDropDownMenu(dropItem) {
+function updateDropDownMenu(dropItem, button) {
   setTimeout(() => {
     dropItems.forEach((item) => {
       item.classList.remove("show");
+      if (button) {
+        button.classList.remove("green");
+      }
     });
     if (showIndex > 0) {
       dropItem.classList.add("show");
     }
   }, 1);
 }
+
+function changeBtnColor(id) {}
+
+const nav = document.querySelector(".main-menu");
+
+window.onscroll = () => {
+  if (window.pageYOffset > 100) {
+    nav.style.backgroundColor = "#001538ea";
+    nav.style.borderBottom = "2px solid #21c4fd";
+  } else {
+    nav.style.background = "none";
+    nav.style.borderBottom = "0px solid #21c4fd";
+  }
+};
 
 ////////////////////////////////
 // Code pour rendre le logo du site cliquable
